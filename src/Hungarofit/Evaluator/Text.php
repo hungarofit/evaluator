f889d0fbf63514052e984a36bdde4ae90f9fb0e9
lajosbencz <lazos@lazos.me>
2017-10-25 22:04:45 +0200

    deprecate separator

--- src/Hungarofit/Evaluator/Text.php
+++ src/Hungarofit/Evaluator/Text.php
@@ -5,21 +5,21 @@ namespace Hungarofit\Evaluator;
 
 class Text
 {
-    public static function camelcase($text, $separator='-')
+    public static function camelcase($text)
     {
         $text = ucfirst($text);
-        return preg_replace_callback('/'.$separator.'([a-z0-9]+)/', function($m) {
+        return preg_replace_callback('/-([a-z0-9]+)/', function($m) {
             return ucfirst($m[1]);
         }, $text);
     }
-    public static function kebabcase($text, $separator='-')
+    public static function kebabcase($text)
     {
         if(!$text) {
             return $text;
         }
         $text[0] = strtolower($text[0]);
-        return preg_replace_callback('/([A-Z]|[0-9][a-z0-9]+)/', function($m) use($separator) {
-            return $separator . strtolower($m[1]);
+        return preg_replace_callback('/([A-Z]|[0-9][a-z0-9]+)/', function($m) {
+            return '-' . strtolower($m[1]);
         }, $text);
     }
 }
