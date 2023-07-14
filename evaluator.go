175eb215993c4b7407e6b8dc393bd8f79a216692
lazos <lazos@lazos.me>
2023-07-15 00:26:01 +0200

    sanity

--- evaluator.go
+++ evaluator.go
@@ -1,10 +1,17 @@
 package evaluator
 
+import "fmt"
+
 func Evaluate(challenge Challenge, gender Gender, age Age, results map[Exercise]ResultValue) (ChallengeScore, error) {
 	cs := ChallengeScore{
 		Total:     0,
 		TotalMax:  0,
 		Exercises: map[Exercise]ChallengeScoreExercise{},
+	}
+	for _, ex := range challenge.GetExercizes() {
+		if _, ok := results[ex]; !ok {
+			return cs, fmt.Errorf("missing result for exercise: " + ex.GetName())
+		}
 	}
 	for ex, rv := range results {
 		s, err := TableEvaluate(ex, gender, age, rv)
